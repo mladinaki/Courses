06a24e19d7973a5e424afc162cdeb72602d00ea9
mladinaki <mladinaki54@gmail.com>
2023-09-23 10:44:30 +0300

    CUB-P1 Setup express to work with static files trim handlebars views

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const handlebars = require('express-handlebars');
+const path = require('path');
 const app = express();
 const PORT = 3000;
 
 //Handlebars configurations
 app.engine('hbs',handlebars.engine({extname:'hbs'}));
 app.set('view engine','hbs');
-app.set('views','src/views')
+app.set('views','src/views');
+
+
+//Setup static files
+
+const staticFiles = express.static(path.resolve(__dirname,'public'));
+app.use(staticFiles)
+
+//Setup static files End
 
 
 app.get('/', (req, res) => {
