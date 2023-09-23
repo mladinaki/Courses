9637f35e49d4d1266d1ef75fdbc6ba383e57e756
mladinaki <mladinaki54@gmail.com>
2023-09-23 10:18:25 +0300

    CUB-P1 initialise,handlebars,setup first default view

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const handlebars = require('express-handlebars');
 const app = express();
 const PORT = 3000;
 
+//Handlebars configurations
+app.engine('hbs',handlebars.engine({extname:'hbs'}));
+app.set('view engine','hbs');
+app.set('views','src/views')
+
 
 app.get('/', (req, res) => {
-    res.send('Welcome express!!')
+    res.render('index')
 })
 
 app.listen(PORT, () => console.log(`Server listening on ${PORT}...`));
